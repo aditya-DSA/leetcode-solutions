61a46449d2b269fe300b31d34978ed3eb544694b
Alluri Aditya <your_email@gmail.com>
2026-03-03 16:44:10 +0530

    Added Subarray Sum Divides by K Solution

---
+++ PrefixSum/974_SubarraySumDividesByK.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        //create a new hash map
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int i =0;i<nums.length;i++) {
+            sum=sum+nums[i];
+            int rem = sum%k;
+            if(rem<0) {
+                rem = rem+k;
+            }
+            if(map.containsKey(rem)) {
+                count+=map.get(rem);
+            }
+            map.put(rem,map.getOrDefault(rem,0)+1);
+        }
+        return count;
+    }
+}
